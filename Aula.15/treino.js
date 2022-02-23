c261061d0611a04888b80063adc2de24ee3d3caa
Mrodrigues02 <86750502+Mrodrigues02@users.noreply.github.com>
2022-02-23 10:37:32 -0300

    Aula.15

---
+++ Aula.15/treino.js
@@ -0,0 +1,12 @@
+let num = [5,8,4,3,2,1,7]
+
+/*for(var pos = 0; pos < num.length; pos++)
+console.log(`Posição ${pos} tem o valor ${num[pos]}`)*/
+/*for(let pos in num) {
+    console.log(num[pos])
+}*/
+let mostar = num.sort()
+console.log(mostar)
+let pos = num.indexOf(5)
+console.log(pos)
+
